bff8114195e80d01472996aeb488a5a65791cefb
Tamer <almasri.tamer100@gmail.com>
2024-12-03 16:23:06 +0100

    finit interface jouer, infos, et interface principale

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from flask import Flask, render_template, session, redirect
 
-app = FastAPI()
+app = Flask(__name__)
+app.secret_key = "e^31!+c1#5t)g1riwa6xq&)zt4xo5h6evpxr7r_xsu_n*r#s3f"
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.route("/jouer")
+def jouer():
+    return render_template("jouer.html")
+
+@app.route("/infos")
+def infos():
+    return render_template("infos.html")
